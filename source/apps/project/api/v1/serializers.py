817580256da606ae193641167ec5bbcea8b1cca5
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-19 15:32:27 +0330

    create serializer for task

--- source/apps/project/api/v1/serializers.py
+++ source/apps/project/api/v1/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from apps.project.models import Project
+from apps.project.models import Project, Task
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['project'] = ProjectSerializer(
+            Project.objects.get(pk=instance.project.pk)).data
+        return data
