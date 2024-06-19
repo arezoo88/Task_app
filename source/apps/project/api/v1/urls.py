ae4101625c4c4e4e24980756e53bc32aa54c60a3
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-19 15:32:10 +0330

    create router for task views

--- source/apps/project/api/v1/urls.py
+++ source/apps/project/api/v1/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from apps.project.api.v1.views import ProjectViewSet
+from apps.project.api.v1.views import ProjectViewSet, TaskViewSet
 from rest_framework import routers
 
 app_name = 'project.api.v1'
 router = routers.DefaultRouter()
-router.register(r'', ProjectViewSet, basename='project')
+router.register(r'projects', ProjectViewSet, basename='project')
+router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('', include(router.urls)),
-
 ]
