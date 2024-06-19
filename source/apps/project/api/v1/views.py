eddfb41a97d4ef9fe5cebb6c017e8aa2ae8545b7
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-19 15:31:37 +0330

    create api for tasks

--- source/apps/project/api/v1/views.py
+++ source/apps/project/api/v1/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
-from apps.project.models import Project
-from apps.project.api.v1.serializers import ProjectSerializer
+from apps.project.models import Project, Task, Comment
+from apps.project.api.v1.serializers import ProjectSerializer, TaskSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
