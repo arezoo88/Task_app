b68b32485026d311b6892ad1d9cee9c7b03cac7c
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-19 19:30:16 +0330

    create apis for comment

--- source/apps/project/api/v1/views.py
+++ source/apps/project/api/v1/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from apps.project.models import Project, Task, Comment
-from apps.project.api.v1.serializers import ProjectSerializer, TaskSerializer
+from apps.project.api.v1.serializers import ProjectSerializer, TaskSerializer, CommentSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -11,3 +13,19 @@ class ProjectViewSet(viewsets.ModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.filter(task__pk=self.kwargs['id'])
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['task'] = self.kwargs.get('id')
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
