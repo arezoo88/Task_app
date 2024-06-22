6b0dd7b2c5f7c61aea29cacd709e1938308c0b9d
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-22 10:17:56 +0330

    read data from cache in list api project

--- source/apps/project/api/v1/views.py
+++ source/apps/project/api/v1/views.py
@@ -3,11 +3,26 @@ from apps.project.models import Project, Task, Comment
 from apps.project.api.v1.serializers import ProjectSerializer, TaskSerializer, CommentSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from django.conf import settings
+from django.core.cache import cache
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', 60 * 15)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        cache_key = 'projects_list'
+        cached_projects = cache.get(cache_key)
+
+        if cached_projects:
+            return Response(cached_projects)
+        serializer = self.get_serializer(self.queryset, many=True)
+        cache.set(cache_key, serializer.data, CACHE_TTL)
+
+        return Response(serializer.data)
 
 
 class TaskViewSet(viewsets.ModelViewSet):
