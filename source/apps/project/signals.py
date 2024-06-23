86057503f139be3c1f8f885886d2593947dd7578
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-23 09:17:04 +0330

    invalidate tasks cache when tasks change or delete

--- source/apps/project/signals.py
+++ source/apps/project/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from apps.project.models import Project
+from apps.project.models import Project, Task
 from django.core.cache import cache
 
 
@@ -8,3 +8,9 @@ from django.core.cache import cache
 @receiver(post_delete, sender=Project)
 def invalidate_project_cache(sender, instance, **kwargs):
     cache.delete('projects_list')
+
+
+@receiver(post_save, sender=Task)
+@receiver(post_delete, sender=Task)
+def invalidate_task_cache(sender, instance, **kwargs):
+    cache.delete('tasks_list')
