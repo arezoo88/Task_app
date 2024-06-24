4d25fe20024267256ae1e731ca42d10f9ce39833
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-24 08:36:09 +0330

    create tests for project view

--- source/apps/project/tests.py
+++ source/apps/project/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Project, Task, Comment
 
-# Create your tests here.
+
+class ProjectTests(APITestCase):
+    def setUp(self):
+        self.project = Project.objects.create(name="Test Project")
+        self.project_list_url = '/api/v1/projects/'
+        self.project_detail_url = f'/api/v1/projects/{self.project.pk}/'
+
+    def test_list_projects(self):
+        response = self.client.get(self.project_list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_project(self):
+        response = self.client.get(self.project_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_project(self):
+        data = {'name': 'New Project'}
+        response = self.client.post(self.project_list_url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_update_project(self):
+        data = {'name': 'Updated Project'}
+        response = self.client.put(self.project_detail_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_project(self):
+        response = self.client.delete(self.project_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
