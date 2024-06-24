3696f6fdd85a47a3dd266ee1353a4cb857542877
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-24 13:26:27 +0330

    add swagger url

--- source/base/urls.py
+++ source/base/urls.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.project.api.v1.urls', namespace='project.v1')),
 
 ]
+# This is only needed when using runserver.(debug is true)
+if settings.DEBUG:
+    from drf_yasg import openapi
+    from drf_yasg.views import get_schema_view
+    from rest_framework.permissions import AllowAny
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title=f'{settings.SITE["NAME"]} APIs',
+            default_version=f'{settings.REST_FRAMEWORK["DEFAULT_VERSION"]}',
+            description=f'{settings.SITE["DESCRIPTION"]}',
+            contact=openapi.Contact(email=f'{settings.DEFAULT_FROM_EMAIL}')
+        ),
+        public=True,
+        permission_classes=(AllowAny,)
+    )
+    urlpatterns += [
+        path('api-auth',
+             include('rest_framework.urls', namespace='rest_framework')),
+        path('swagger', schema_view.with_ui('swagger', cache_timeout=0),
+             name='schema-swagger-ui'),
+        path('redoc', schema_view.with_ui('redoc', cache_timeout=0),
+             name='schema-redoc')
+    ]
