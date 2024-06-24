bf915aa2928a45b05187f0a8e2967402d902dfaf
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-24 09:09:31 +0330

    cutomize admin panel

--- source/apps/project/admin.py
+++ source/apps/project/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from apps.project.models import Project, Task, Comment
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name',)
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'comment_count')
+    inlines = [CommentInline]
+
+    def comment_count(self, obj):
+        return obj.comments.count()
+    comment_count.short_description = 'Comment Count'
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Project, ProjectAdmin)
