2c1120dd61271dc4698f827e7dd847558bff7da9
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-26 09:42:24 +0330

    add / to url of swagger

--- source/base/urls.py
+++ source/base/urls.py
@@ -27,7 +27,7 @@ if settings.DEBUG:
     urlpatterns += [
         path('api-auth',
              include('rest_framework.urls', namespace='rest_framework')),
-        path('swagger', schema_view.with_ui('swagger', cache_timeout=0),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
              name='schema-swagger-ui'),
         path('redoc', schema_view.with_ui('redoc', cache_timeout=0),
              name='schema-redoc')
