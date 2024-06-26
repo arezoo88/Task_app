5d69d28b8948d7488a10f5acae5e5c0eb0681203
Arezoo <arezoo.darvishi69@yahoo.com>
2024-06-26 10:24:56 +0330

    send realtime notification whr project or task delete or create

--- source/apps/project/signals.py
+++ source/apps/project/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from apps.project.models import Project, Task
 from django.core.cache import cache
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 
 @receiver(post_save, sender=Project)
@@ -14,3 +16,21 @@ def invalidate_project_cache(sender, instance, **kwargs):
 @receiver(post_delete, sender=Task)
 def invalidate_task_cache(sender, instance, **kwargs):
     cache.delete('tasks_list')
+
+
+channel_layer = get_channel_layer()
+
+
+@receiver(post_save, sender=Task)
+@receiver(post_delete, sender=Task)
+@receiver(post_save, sender=Project)
+@receiver(post_delete, sender=Project)
+def send_notification(sender, instance, **kwargs):
+    if kwargs.get('created') or kwargs.get('deleted'):
+        async_to_sync(channel_layer.group_send)(
+            'notifications_group',
+            {
+                'type': 'send_notification',
+                'message': f'{sender.__name__} {instance.id} has been {kwargs.get("created", "deleted")}.'
+            }
+        )
